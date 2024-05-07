1899fd3a12f08595835f0f9f9d6da125f5c51113
DELL <DELL@DESKTOP-04S95NF>
2024-05-07 22:19:36 +0530

    Add project files.

---
+++ MVCWebApiServices/Controllers/TestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MVCWebApiServices.Controllers
+{
+    public class TestController : ApiController
+    {
+
+       static List<string> Colors = new List<string>() { "red", "yellow", "Blue", "pink", "green" };
+
+        public IEnumerable<string> Get()
+        {
+            return Colors;
+        }
+
+        public string Get(int id) 
+        {
+        return Colors[id];
+        }
+
+        public void Post([FromBody] string data)
+        {
+        Colors.Add(data);
+        }
+
+        public void Delete(int id)
+        {
+            Colors.Remove(Colors[id]);
+        }
+
+        public void Put(int id, [FromBody] string data)
+        {
+            Colors[id] = data;
+        }
+
+
+    }
+
+    
+}
---
+++ MVCWebApiServices/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCWebApiServices.WebApiApplication" Language="C#" %>
